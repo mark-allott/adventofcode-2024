33a5fa8bbf62c2af1356c6a4bea560fbccfd9e07
Mark Allott <github.mark-allott@malsoft.com>
2025-02-14 11:34:03 +0000

    Implement running latest, all and specific daily challenges

--- AdventOfCode/DailyChallengeRunner.cs
+++ AdventOfCode/DailyChallengeRunner.cs
@@ -17,12 +17,38 @@ public class DailyChallengeRunner
 
 	public Task StartAsync(CancellationToken cancellationToken)
 	{
+		//	Try to find out what needs to be run
+		var x = Configuration["run"];
 		var challenges = GetDailyChallenges();
 
-		//	TODO: implement different options
+		//	run using the different options
+		if (string.IsNullOrWhiteSpace(x))
+		{
+			//	Nothing specific is supplied, so execute the challenge with the greatest day number value
+			RunChallenge(challenges.Last());
+		}
+		else if (x.Equals("all", StringComparison.OrdinalIgnoreCase))
+		{
+			//	All challenges are requested, so run them in DayNumber order
+			challenges.ForEach(c => RunChallenge(c));
+		}
+		else
+		{
+			//	If here then we assume a list of days is present, so we need to
+			//	convert to integers to find out what these days are
+			var days = x.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(s => int.TryParse(s, out var result) ? result : int.MinValue)
+				.Where(q => q != int.MinValue)
+				.OrderBy(o => o)
+				.ToList();
 
-		//	Execute the challenge with the greatest day number value
-		RunChallenge(challenges.Last());
+			//	Filter the challenges to be run that match any specified days
+			var challengesToRun = challenges.Where(c => days.Contains(c.DayNumber))
+				.ToList();
+
+			//	Run the requested daily challenges
+			challengesToRun.ForEach(c => RunChallenge(c));
+		}
 
 		return Task.CompletedTask;
 	}
@@ -45,6 +71,10 @@ public class DailyChallengeRunner
 
 	#endregion
 
+	/// <summary>
+	/// Provide a list of the challenges that the application contains in ascending <see cref="IDailyChallenge.DayNumber"/> order
+	/// </summary>
+	/// <returns>All challenges registered in <see cref="ServiceProvider"/></returns>
 	private List<IDailyChallenge> GetDailyChallenges()
 	{
 		//	Assuming the services provider is valid, obtain a list of daily challenges ordered by day number
@@ -53,14 +83,29 @@ public class DailyChallengeRunner
 			.ToList();
 	}
 
-	private void RunChallenge(IDailyChallenge challenge)
+	/// <summary>
+	/// Executes the specified challenge, sending output to the console
+	/// </summary>
+	/// <param name="challenge">The challenge being executed</param>
+	/// <exception cref="ArgumentNullException">
+	private static void RunChallenge(IDailyChallenge challenge)
 	{
+		//	No challenge?? Throw an exception
 		ArgumentNullException.ThrowIfNull(challenge, nameof(challenge));
+
+		//	Write some output to the console stating which challenge is being run
 		Console.WriteLine($"Running challenge for day {challenge.DayNumber}");
+
+		//	Run the challenge, state whether it was completed sucessfully
+		//	(both part one and part two must be completed successfully to be
+		//	stated as completely successful) and display the results
 		var result = challenge.Execute();
 		Console.WriteLine($"Challenge completed {(result ? "" : "un")}successfully");
 		Console.WriteLine($"Part One solution: {challenge.PartOneResult}");
 		Console.WriteLine($"Part Two solution: {challenge.PartTwoResult}");
+
+		//	Print a separator line to distinguish between the end of this
+		//	challenge and any challenges following it
 		Console.WriteLine("{0}{1}{2}{1}{0}", new String('=', 5), new String(' ', 5), new String('-', 60));
 	}
 }
