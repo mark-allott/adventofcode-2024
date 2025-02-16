ee1df41b7b0e3a0689d8d32c183f26b488c0e757
Mark Allott <github.mark-allott@malsoft.com>
2025-02-16 17:08:33 +0000

    Add skeleton code for day 3

---
+++ AdventOfCode/Challenges/Day03.cs
@@ -0,0 +1,16 @@
+using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.Challenges;
+
+public partial class Day03
+	: AbstractDailyChallenge, IAutoRegister
+{
+	#region Overrides of AbstractDailyChallenge
+
+	public Day03()
+		: base(3, "day03-input.txt")
+	{
+	}
+
+	#endregion
+}
