c137468e13a8258a4c54a45faffef0517c32b762
Mark Allott <github.mark-allott@malsoft.com>
2025-02-20 11:15:19 +0000

    Add skeleton for day 7

---
+++ AdventOfCode/Challenges/Day07.cs
@@ -0,0 +1,16 @@
+using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.Challenges;
+
+public partial class Day07
+	: AbstractDailyChallenge, IAutoRegister
+{
+	#region Overrides of AbstractDailyChallenge
+
+	public Day07()
+		: base(7, "day07-input.txt")
+	{
+	}
+
+	#endregion
+}
