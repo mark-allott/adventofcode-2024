8dd9be770f63c8943abf39f5e1dbd4bc36bf796b
Mark Allott <github.mark-allott@malsoft.com>
2025-02-28 13:39:21 +0000

    Add skeleton for day 10 challenge

---
+++ AdventOfCode/Challenges/Day10/Day10.cs
@@ -0,0 +1,16 @@
+using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.Challenges.Day10;
+
+public partial class Day10
+	: AbstractDailyChallenge, IAutoRegister
+{
+	#region Overrides of AbstractDailyChallenge
+
+	public Day10()
+		: base(10, "day10-input.txt")
+	{
+	}
+
+	#endregion
+}
