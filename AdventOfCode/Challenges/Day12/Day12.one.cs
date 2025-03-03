6a77fc964357abfb4913ca5cc8b6051408e9963b
Mark Allott <github.mark-allott@malsoft.com>
2025-03-03 11:15:08 +0000

    Solution for part one of challenge

--- AdventOfCode/Challenges/Day12/Day12.one.cs
+++ AdventOfCode/Challenges/Day12/Day12.one.cs
@@ -17,7 +17,11 @@ public partial class Day12
 	{
 		LoadAndReadFile();
 
-		long total = 0;
+		var garden = new Garden(InputFileLines);
+		garden.SetRegions();
+		garden.SetFences();
+
+		long total = garden.Regions.Sum(r => r.Cost);
 		PartOneResult = $"Cost of fencing = {total}";
 		return true;
 	}
@@ -33,7 +37,38 @@ public partial class Day12
 	{
 		var inputCount = _partOneTestInput.Count;
 		Debug.Assert(inputCount > 0);
+		Debug.Assert(inputCount == _partOneExpectedRegions.Count);
+		Debug.Assert(inputCount == _partOneExpectedAreas.Count);
+		Debug.Assert(inputCount == _partOneExpectedPerimeters.Count);
 		Debug.Assert(inputCount == _partOneExpectedCosts.Count);
+
+		for (var i = 0; i < inputCount; i++)
+		{
+			var input = _partOneTestInput[i];
+
+			var garden = new Garden(input);
+			garden.SetRegions();
+			garden.SetFences();
+
+			var regionCount = garden.Regions.Count;
+
+			Debug.Assert(_partOneExpectedRegions[i] == regionCount);
+
+			var expectedAreas = _partOneExpectedAreas[i];
+			var expectedPerimeters = _partOneExpectedPerimeters[i];
+			var expectedCost = _partOneExpectedCosts[i];
+
+			Debug.Assert(expectedAreas.Count == regionCount);
+			Debug.Assert(expectedPerimeters.Count == regionCount);
+
+			for (var r = 0; r < garden.Regions.Count; r++)
+			{
+				var region = garden.Regions[r];
+				Debug.Assert(expectedAreas[r] == region.Area);
+				Debug.Assert(expectedPerimeters[r] == region.Perimeter);
+			}
+			Debug.Assert(expectedCost == garden.Regions.Sum(r => r.Cost));
+		}
 	}
 
 	/// <summary>
@@ -44,6 +79,25 @@ public partial class Day12
 		new List<string>(){"AAAA","BBCD","BBCC","EEEC"},
 		new List<string>(){"OOOOO","OXOXO","OOOOO","OXOXO","OOOOO"},
 		new List<string>(){"RRRRIICCFF","RRRRIICCCF","VVRRRCCFFF","VVRCCCJFFF","VVVVCJJCFE","VVIVCCJJEE","VVIIICJJEE","MIIIIIJJEE","MIIISIJEEE","MMMISSJEEE"}
+	};
+
+	private List<int> _partOneExpectedRegions = new List<int>()
+	{
+		5, 5, 11
+	};
+
+	private List<List<int>> _partOneExpectedAreas = new List<List<int>>()
+	{
+		new(){ 4, 4, 4, 1, 3 },
+		new(){ 21, 1, 1, 1, 1 },
+		new(){ 12, 4, 14, 10, 13, 11, 1, 13, 14, 5, 3 }
+	};
+
+	private List<List<int>> _partOneExpectedPerimeters = new List<List<int>>()
+	{
+		new(){ 10, 8, 10, 4, 8 },
+		new(){ 36, 4, 4, 4, 4 },
+		new(){ 18, 8, 28, 18, 20, 20, 4, 18, 22, 12, 8 }
 	};
 
 	private List<int> _partOneExpectedCosts = new List<int>()
