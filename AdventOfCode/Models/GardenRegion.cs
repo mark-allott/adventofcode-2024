1c104fb2573baefe654821d2d48920ec1e08583b
Mark Allott <github.mark-allott@malsoft.com>
2025-03-05 15:23:40 +0000

    Refactor to calculate sides

--- AdventOfCode/Models/GardenRegion.cs
+++ AdventOfCode/Models/GardenRegion.cs
@@ -31,6 +31,16 @@ internal class GardenRegion
 	/// </summary>
 	public int Cost => Area * Perimeter;
 
+	/// <summary>
+	/// Property to return the number of sides within the region
+	/// </summary>
+	public int Sides => GetSides();
+
+	/// <summary>
+	/// New property to calculate the bulk cost for the region
+	/// </summary>
+	public int BulkCost => Area * Sides;
+
 	#endregion
 
 	#region ctor
@@ -103,6 +113,93 @@ internal class GardenRegion
 	public override string ToString()
 	{
 		return $"{nameof(Area)}: {Area}, {nameof(Perimeter)}: {Perimeter}";
+	}
+
+	/// <summary>
+	/// Helper class to calculate sides for a given grouping
+	/// </summary>
+	private class FenceLocation
+	{
+		public int Key { get; }
+		public FencePosition Position { get; }
+		public List<int> Locations { get; }
+
+		public FenceLocation(int key, FencePosition position, IEnumerable<MapCoord> locations)
+		{
+			var horizontalPositions = FencePosition.Top | FencePosition.Bottom;
+			Key = key;
+			Position = position;
+			Locations = (position & horizontalPositions) > 0
+				? locations.Select(s => s.X).OrderBy(o => o).ToList()
+				: locations.Select(s => s.Y).OrderBy(o => o).ToList();
+		}
+
+		public int Sides => GetSides();
+
+		private int GetSides()
+		{
+			//	If zero or one location, return the count, nothing else to work out
+			if (Locations.Count < 2)
+				return Locations.Count;
+
+			//	initialise counters
+			var sides = 0;
+			var lastLocation = -2;
+			foreach (var location in Locations)
+			{
+				//	If a side it continuous, then the locations increment by
+				//	one each time. If a gap appears, then we have a new side present
+				if (location > lastLocation + 1)
+					sides++;
+				lastLocation = location;
+			}
+			return sides;
+		}
+	}
+
+	/// <summary>
+	/// Helper method to identify the number of sides of fences within the region
+	/// </summary>
+	/// <returns>The number of sides found</returns>
+	/// <remarks>
+	/// To locate the sides within the region, first split all of the existing
+	/// fences into their component parts, ensure all fence locations are grouped
+	/// by common location elements: top/bottom by row and left/right by column,
+	/// then in ascending corresponding orders to allow detection of gaps in the
+	/// sequences more easily.
+	/// </remarks>
+	private int GetSides()
+	{
+		//	No plots, no sides
+		if (_plots.Count == 0)
+			return 0;
+
+		//	Get the plots where there are "top" fences
+		var topFences = _plots
+			.Where(p => (p.FencePositions & FencePosition.Top) > 0)
+			.GroupBy(p => p.Location.Y)
+			.Select(g => new FenceLocation(g.Key, FencePosition.Top, g.Select(s => s.Location)))
+			.Sum(s => s.Sides);
+
+		var bottomFences = _plots
+			.Where(p => (p.FencePositions & FencePosition.Bottom) > 0)
+			.GroupBy(p => p.Location.Y)
+			.Select(g => new FenceLocation(g.Key, FencePosition.Bottom, g.Select(s => s.Location)))
+			.Sum(s => s.Sides);
+
+		var leftFences = _plots
+			.Where(p => (p.FencePositions & FencePosition.Left) > 0)
+			.GroupBy(p => p.Location.X)
+			.Select(g => new FenceLocation(g.Key, FencePosition.Left, g.Select(s => s.Location)))
+			.Sum(s => s.Sides);
+
+		var rightFences = _plots
+			.Where(p => (p.FencePositions & FencePosition.Right) > 0)
+			.GroupBy(p => p.Location.X)
+			.Select(g => new FenceLocation(g.Key, FencePosition.Right, g.Select(s => s.Location)))
+			.Sum(s => s.Sides);
+
+		return topFences + bottomFences + leftFences + rightFences;
 	}
 
 	#endregion
