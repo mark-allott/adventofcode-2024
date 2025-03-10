12e4622164dfa6fb982131a56480041523bde6f6
Mark Allott <github.mark-allott@malsoft.com>
2025-03-10 16:25:56 +0000

    Refactor: add coalescing expression to stop CS8602 warning showing up

--- AdventOfCode/Models/MapCoord.cs
+++ AdventOfCode/Models/MapCoord.cs
@@ -98,7 +98,7 @@ internal class MapCoordEqualityComparer
 	{
 		return b is null
 			? a is null
-			: a.X == b.X && a.Y == b.Y;
+			: a?.X == b.X && a.Y == b.Y;
 	}
 
 	public int GetHashCode([DisallowNull] MapCoord obj)
