96e6bd1efd77f3c2867599458bb248e1e4bf71a4
Mark Allott <github.mark-allott@malsoft.com>
2025-03-12 13:20:16 +0000

    Add partial solution for part one: solves examples but main problem has recursion problems

--- AdventOfCode/Challenges/Day16/Day16.one.cs
+++ AdventOfCode/Challenges/Day16/Day16.one.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics;
-using System.Text.RegularExpressions;
 using AdventOfCode.Interfaces;
 using AdventOfCode.Models;
 
@@ -18,7 +17,13 @@ public partial class Day16
 	{
 		LoadAndReadFile();
 
-		long total = 0;
+		var maze = new ReindeerMaze();
+		maze.Load(InputFileLines);
+		var completed = maze.FindRoutes()
+			.Where(q => q.CompletesMaze)
+			.OrderBy(o => o.Score)
+			.ToList();
+		long total = completed[0].Score;
 		PartOneResult = $"Reindeer Maze lowest score = {total}";
 		return true;
 	}
@@ -32,10 +37,66 @@ public partial class Day16
 	/// </summary>
 	public void PartOneTest()
 	{
+		for (var i = 0; i < _partOneTestInput.Count; i++)
+		{
+			var maze = new ReindeerMaze();
+			maze.Load(_partOneTestInput[i]);
+			var routes = maze.FindRoutes();
+			var completed = routes.Where(q => q.CompletesMaze).OrderBy(o => o.Score).ToList();
+			var scores = completed.Select(s => s.Score).ToList();
+			var minScore = scores.Min();
+			Debug.Assert(minScore > 0);
+			Debug.Assert(_partOneExpectedScores[i] == minScore);
+			Console.Write(maze.ToString());
+			Console.WriteLine(completed[0].Print());
+		}
 	}
 
-	private List<string> _partOneTestInput = new List<string>()
+	private List<List<string>> _partOneTestInput = new List<List<string>>()
 	{
+		new List<string>()
+		{
+			"###############",
+			"#.......#....E#",
+			"#.#.###.#.###.#",
+			"#.....#.#...#.#",
+			"#.###.#####.#.#",
+			"#.#.#.......#.#",
+			"#.#.#####.###.#",
+			"#...........#.#",
+			"###.#.#####.#.#",
+			"#...#.....#.#.#",
+			"#.#.#.###.#.#.#",
+			"#.....#...#.#.#",
+			"#.###.#.#.#.#.#",
+			"#S..#.....#...#",
+			"###############"
+		},
+		new List<string>()
+		{
+			"#################",
+			"#...#...#...#..E#",
+			"#.#.#.#.#.#.#.#.#",
+			"#.#.#.#...#...#.#",
+			"#.#.#.#.###.#.#.#",
+			"#...#.#.#.....#.#",
+			"#.#.#.#.#.#####.#",
+			"#.#...#.#.#.....#",
+			"#.#.#####.#.###.#",
+			"#.#.#.......#...#",
+			"#.#.###.#####.###",
+			"#.#.#...#.....#.#",
+			"#.#.#.#####.###.#",
+			"#.#.#.........#.#",
+			"#.#.#.#########.#",
+			"#S#.............#",
+			"#################"
+		}
+	};
+
+	private List<int> _partOneExpectedScores = new List<int>()
+	{
+		7036, 11048
 	};
 
 	#endregion
