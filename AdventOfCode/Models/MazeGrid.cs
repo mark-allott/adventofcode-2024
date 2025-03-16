7594cd21419732ad4e4bc5543ba05c967cc1a2be
Mark Allott <github.mark-allott@malsoft.com>
2025-03-16 17:03:35 +0000

    Refactor: make naming more generic

---
+++ AdventOfCode/Models/MazeGrid.cs
@@ -0,0 +1,32 @@
+using AdventOfCode.Enums;
+using AdventOfCode.Extensions;
+using AdventOfCode.Interfaces;
+
+namespace AdventOfCode.Models;
+
+internal class MazeGrid
+	: GenericGrid<MazeCellType>
+{
+	#region ctor
+
+	public MazeGrid(int x, int y)
+		: base(x, y, new MazeCellTypeRenderer())
+	{
+	}
+
+	public MazeGrid(MapCoord coords)
+		: base(coords, new MazeCellTypeRenderer())
+	{
+	}
+
+	#endregion
+}
+
+internal class MazeCellTypeRenderer
+	: ICellRenderer<MazeCellType>
+{
+	public char ToCharacter(MazeCellType value)
+	{
+		return value.ToCharacter();
+	}
+}
