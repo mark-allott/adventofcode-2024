5975197a554604905352c07758312dd92566e8fd
Mark Allott <github.mark-allott@malsoft.com>
2025-03-20 16:42:38 +0000

    New interface to describe a node within a maze class, composing of location and distance attributes

---
+++ AdventOfCode/Interfaces/IMazeNode.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode.Interfaces;
+
+internal interface IMazeNode
+{
+	/// <summary>
+	/// Represents the coords in a graph as X & Y components
+	/// </summary>
+	IGraphCoordinate Location { get; }
+
+	/// <summary>
+	/// Represents the distance between one node and another
+	/// </summary>
+	int Distance { get; }
+}
