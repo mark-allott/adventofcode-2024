70f4539743fd3777903536d7b56a785d2d94cbce
Mark Allott <github.mark-allott@malsoft.com>
2025-03-21 11:48:56 +0000

    Add setter to properties

--- AdventOfCode/Interfaces/IMazeNode.cs
+++ AdventOfCode/Interfaces/IMazeNode.cs
@@ -5,10 +5,10 @@ internal interface IMazeNode
 	/// <summary>
 	/// Represents the coords in a graph as X & Y components
 	/// </summary>
-	IGraphCoordinate Location { get; }
+	IGraphCoordinate Location { get; set; }
 
 	/// <summary>
 	/// Represents the distance between one node and another
 	/// </summary>
-	int Distance { get; }
+	int Distance { get; set; }
 }
