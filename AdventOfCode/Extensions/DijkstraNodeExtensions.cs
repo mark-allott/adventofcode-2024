8b827faf7bd6cb62167c5b00dd0c7c5471508395
Mark Allott <github.mark-allott@malsoft.com>
2025-03-24 12:27:31 +0000

    New method to get neighbouring nodes; refactor to use distance strategy

--- AdventOfCode/Extensions/DijkstraNodeExtensions.cs
+++ AdventOfCode/Extensions/DijkstraNodeExtensions.cs
@@ -1,4 +1,5 @@
 using AdventOfCode.Enums;
+using AdventOfCode.Interfaces;
 using AdventOfCode.Models;
 
 namespace AdventOfCode.Extensions;
@@ -13,11 +14,13 @@ internal static class DijkstraNodeExtensions
 	/// <param name="node">The current node being examined</param>
 	/// <param name="maze">The maze being traversed</param>
 	/// <param name="unvisitedNodes">The list of unvisited nodes</param>
-	public static void UpdateNeighbours(this DijkstraNode node, MazeGrid maze, List<DijkstraNode> unvisitedNodes)
+	/// <param name="distanceStrategy">The strategy used to calculate the new distance from the current node</param>
+	public static void UpdateNeighbours(this DijkstraNode node, MazeGrid maze, List<DijkstraNode> unvisitedNodes, IDijkstraDistanceStrategy<MazeMovement> distanceStrategy)
 	{
 		ArgumentNullException.ThrowIfNull(node, nameof(node));
 		ArgumentNullException.ThrowIfNull(maze, nameof(maze));
 		ArgumentNullException.ThrowIfNull(unvisitedNodes, nameof(unvisitedNodes));
+		ArgumentNullException.ThrowIfNull(distanceStrategy, nameof(distanceStrategy));
 
 		var movesToCheck = new List<MazeMovement>() { MazeMovement.GoForward, MazeMovement.TurnLeft, MazeMovement.TurnRight };
 
@@ -38,11 +41,7 @@ internal static class DijkstraNodeExtensions
 				continue;
 
 			//	Calculate the new distance value for the neighbour
-			var newDistance = node.Distance + moveToCheck switch
-			{
-				MazeMovement.GoForward => 1,
-				_ => 1001
-			};
+			var newDistance = node.Distance + distanceStrategy.GetDistance(moveToCheck);
 
 			//	If the new distance is less than the current value, update the neighbour
 			if (newDistance < neighbour.Distance)
@@ -51,5 +50,38 @@ internal static class DijkstraNodeExtensions
 				neighbour.Direction = travelDirection;
 			}
 		}
+	}
+
+	/// <summary>
+	/// Returns a list of neighbouring cells for <paramref name="node"/>, in a <paramref name="maze"/> based on the list of nodes in <paramref name="unvisitedNodes"/>
+	/// </summary>
+	/// <param name="node">The current node</param>
+	/// <param name="maze">The overall maze</param>
+	/// <param name="unvisitedNodes">A list of nodes that have not been visited yet</param>
+	/// <returns>A list of nodes that are neighbours of <paramref name="node"/> and have not been visited previously</returns>
+	public static List<DijkstraNode> GetNeighbours(this DijkstraNode node, MazeGrid maze, List<DijkstraNode> unvisitedNodes)
+	{
+		ArgumentNullException.ThrowIfNull(node, nameof(node));
+		ArgumentNullException.ThrowIfNull(maze, nameof(maze));
+		ArgumentNullException.ThrowIfNull(unvisitedNodes, nameof(unvisitedNodes));
+
+		var neighbours = new List<DijkstraNode>();
+
+		var movesToCheck = new List<MazeMovement>() { MazeMovement.GoForward, MazeMovement.TurnLeft, MazeMovement.TurnRight };
+
+		foreach (var moveToCheck in movesToCheck)
+		{
+			var travelDirection = moveToCheck.ToDirectionOfTravel(node.Direction);
+			var offset = travelDirection.ToMapCoordOffset();
+			var newCoord = node.Location.OffsetBy(offset.yOffset, offset.xOffset);
+
+			//	Find our neighbour from the unvisited nodes
+			var neighbour = unvisitedNodes.FirstOrDefault(q => q.Location.Equals(newCoord));
+
+			//	Check only unvisited nodes, if the neighbour we want is there, return as part of neighbours
+			if (neighbour is not null)
+				neighbours.Add(neighbour);
+		}
+		return neighbours;
 	}
 }
