4b7ce3dbf580bd8efd52cc7b20b0d0d8573b1b10
Mark Allott <github.mark-allott@malsoft.com>
2025-03-26 09:05:18 +0000

    Add tests and solution for part two

--- AdventOfCode/Challenges/Day16/Day16.two.cs
+++ AdventOfCode/Challenges/Day16/Day16.two.cs
@@ -1,10 +1,11 @@
+using System.Diagnostics;
 using AdventOfCode.Interfaces;
 using AdventOfCode.Models;
 
 namespace AdventOfCode.Challenges.Day16;
-/*
+
 public partial class Day16
-	: AbstractDailyChallenge, IAutoRegister
+	: AbstractDailyChallenge, IAutoRegister, IPartTwoTestable
 {
 	#region Overrides to run part two of challenge
 
@@ -16,11 +17,33 @@ public partial class Day16
 	{
 		LoadAndReadFile();
 
-		long total = 0;
-		PartTwoResult = $"Reindeer Maze lowest score = {total}";
+		var maze = new ReindeerMaze();
+		maze.Load(InputFileLines);
+		long total = maze.DijkstraPathSolver();
+		PartTwoResult = $"Reindeer Maze cells on path = {total}";
 		return true;
 	}
 
 	#endregion
+
+	#region IPartTwoTestable implementation
+
+	public void PartTwoTest()
+	{
+		for (var i = 0; i < _partOneTestInput.Count; i++)
+		{
+			var maze = new ReindeerMaze();
+			maze.Load(_partOneTestInput[i]);
+			var cellsOnPath = maze.DijkstraPathSolver();
+			Debug.Assert(cellsOnPath > 0);
+			Debug.Assert(_partTwoExpectedScores[i] == cellsOnPath);
+		}
+	}
+
+	private readonly List<int> _partTwoExpectedScores = new List<int>()
+	{
+		45, 64
+	};
+
+	#endregion
 }
-*/
