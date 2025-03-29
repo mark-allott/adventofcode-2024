537f371a25ff21d79cf039f2245ba94aaa31ebca
Mark Allott <github.mark-allott@malsoft.com>
2025-03-29 15:36:16 +0000

    Solution for part one of challenge

--- AdventOfCode/Challenges/Day18/Day18.one.cs
+++ AdventOfCode/Challenges/Day18/Day18.one.cs
@@ -1,5 +1,5 @@
 using System.Diagnostics;
-using System.Text.RegularExpressions;
+using AdventOfCode.Extensions;
 using AdventOfCode.Interfaces;
 using AdventOfCode.Models;
 
@@ -18,8 +18,11 @@ public partial class Day18
 	{
 		LoadAndReadFile();
 
-		string output = "N/A";
-		PartOneResult = $"{ChallengeTitle} output = {output}";
+		var corruptedCoords = GetCoordinates(InputFileLines);
+		var maze = new RamRun(71, 71);
+		maze.LoadCorruption(corruptedCoords, 1024);
+		string output = $"{maze.GetShortestPath()}";
+		PartOneResult = $"{ChallengeTitle} minimum steps = {output}";
 		return true;
 	}
 
@@ -32,7 +35,54 @@ public partial class Day18
 	/// </summary>
 	public void PartOneTest()
 	{
+		var corruptedCoords = GetCoordinates(_partOneTestInput);
+		var maze = new RamRun(7, 7);
+		maze.LoadCorruption(corruptedCoords, 12);
+		Console.WriteLine(maze);
+
+		var path = maze.GetShortestPath();
+		Debug.Assert(22 == path);
 	}
+
+	private List<Coordinate> GetCoordinates(IEnumerable<string> input)
+	{
+		var coordinates = new List<Coordinate>();
+		foreach (var line in input)
+		{
+			var values = line.ParseStringToListOfInt();
+			coordinates.Add(new Coordinate(values[1], values[0]));
+		}
+		return coordinates;
+	}
+
+	private List<string> _partOneTestInput = new List<string>()
+	{
+		"5,4",
+		"4,2",
+		"4,5",
+		"3,0",
+		"2,1",
+		"6,3",
+		"2,4",
+		"1,5",
+		"0,6",
+		"3,3",
+		"2,6",
+		"5,1",
+		"1,2",
+		"5,5",
+		"2,5",
+		"6,5",
+		"1,4",
+		"0,4",
+		"6,4",
+		"1,1",
+		"6,1",
+		"1,0",
+		"0,5",
+		"1,6",
+		"2,0"
+	};
 
 	#endregion
 
