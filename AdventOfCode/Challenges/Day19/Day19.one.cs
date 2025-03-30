2b01390cc334e006360352d63c8dd5eb5c4db547
Mark Allott <github.mark-allott@malsoft.com>
2025-03-30 09:48:10 +0100

    Add solution for part one

--- AdventOfCode/Challenges/Day19/Day19.one.cs
+++ AdventOfCode/Challenges/Day19/Day19.one.cs
@@ -18,8 +18,11 @@ public partial class Day19
 	{
 		LoadAndReadFile();
 
-		string output = $"N/A";
-		PartOneResult = $"{ChallengeTitle} designs that are possible = {output}";
+		var (patterns, designs) = ReadData(InputFileLines);
+		var ll = new LinenLayout(patterns, designs);
+		var goodDesigns = ll.GetValidDesigns();
+
+		PartOneResult = $"{ChallengeTitle} designs that are possible = {goodDesigns}";
 		return true;
 	}
 
@@ -32,15 +35,64 @@ public partial class Day19
 	/// </summary>
 	public void PartOneTest()
 	{
+		var (patterns, designs) = ReadData(_partOneTestInput);
+		var ll = new LinenLayout(patterns, designs);
+		var goodDesigns = ll.GetValidDesigns();
+
+		Debug.Assert(6 == goodDesigns);
 	}
 
 	private List<string> _partOneTestInput = new List<string>()
 	{
+		"r, wr, b, g, bwu, rb, gb, br",
+		"",
+		"brwrr",
+		"bggr",
+		"gbbr",
+		"rrbgbr",
+		"ubwu",
+		"bwurrg",
+		"brgr",
+		"bbrgwb"
 	};
 
 	#endregion
 
 	#region Methods
+
+	private (List<string> patterns, List<string> designs) ReadData(IEnumerable<string> data)
+	{
+		var patterns = new List<string>();
+		var designs = new List<string>();
+		var readingPatterns = true;
+
+		//	Read patterns and desired designs
+		foreach (var line in data)
+		{
+			if (string.IsNullOrWhiteSpace(line))
+			{
+				readingPatterns = false;
+				continue;
+			}
+
+			if (readingPatterns)
+			{
+				patterns.AddRange(GetPatterns(line));
+			}
+			else
+			{
+				designs.Add(line);
+			}
+		}
+		return (patterns, designs);
+	}
+
+	public List<string> GetPatterns(string input)
+	{
+		return input.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+			.OrderBy(o => o)
+			.ToList();
+	}
 
 	#endregion
 }
