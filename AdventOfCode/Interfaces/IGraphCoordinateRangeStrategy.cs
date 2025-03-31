c7165c3ae3ef8b3f67952ea3b817a28e1dc4ab25
Mark Allott <github.mark-allott@malsoft.com>
2025-03-31 18:35:29 +0100

    Add comments

--- AdventOfCode/Interfaces/IGraphCoordinateRangeStrategy.cs
+++ AdventOfCode/Interfaces/IGraphCoordinateRangeStrategy.cs
@@ -1,5 +1,8 @@
 namespace AdventOfCode.Interfaces;
 
+/// <summary>
+/// A specific interface implementation of <see cref="IRangeStrategy{T}"/> for <see cref="IGraphCoordinate"/>
+/// </summary>
 internal interface IGraphCoordinateRangeStrategy
 	: IRangeStrategy<IGraphCoordinate>
 {
