08893b0b61b8225929a043be99db5432a0dd138b
Mark Allott <github.mark-allott@malsoft.com>
2025-04-13 15:14:02 +0100

    Solution for part one

--- AdventOfCode/Challenges/Day22/Day22.one.cs
+++ AdventOfCode/Challenges/Day22/Day22.one.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using AdventOfCode.Extensions;
 using AdventOfCode.Interfaces;
 using AdventOfCode.Models;
 
@@ -16,7 +17,14 @@ public partial class Day22
 	protected override bool PartOne()
 	{
 		LoadAndReadFile();
-		var result = "N/A";
+		//	Convert inputs to a list of ints
+		var inputs = InputFileLines.ParseEnumerableOfStringToListOfInt();
+
+		//	Grab a new instance of the model to do some work
+		var monkeyMarket = new MonkeyMarket();
+
+		//	Iterate over the inputs, find the 2000th value for the secret, then provide the sum of all values
+		var result = inputs.Select(i=> monkeyMarket.MutateFor(i,2000)[2000]).Sum();
 		PartOneResult = $"{ChallengeTitle} result = {result}";
 		return true;
 	}
@@ -30,7 +38,49 @@ public partial class Day22
 	/// </summary>
 	public void PartOneTest()
 	{
+		var sut = new MonkeyMarket();
+		var result = sut.MutateFor(123, 10);
+
+		for (var i = 0; i < _partOneExpectedSecrets.Count;)
+		{
+			Debug.Assert(_partOneExpectedSecrets[i] == result[++i]);
+		}
+
+		long sumOfResults = 0L;
+		_partOneTestData.ForEach(i =>
+		{
+			result = sut.MutateFor(i.Item1, i.Item2);
+			var actual = result[i.Item2];
+			sumOfResults += actual;
+			Debug.Assert(actual == i.Item3);
+		});
+		Debug.Assert(37327623 == sumOfResults);
 	}
+
+	/// <summary>
+	/// Part one gives a simple check of for a given initial secret of 123, the next 10 secrets are:
+	/// </summary>
+	private List<long> _partOneExpectedSecrets = new List<long>()
+	{
+		15887950,
+		16495136,
+		527345,
+		704524,
+		1553684,
+		12683156,
+		11100544,
+		12249484,
+		7753432,
+		5908254,
+	};
+
+	private List<(int, int, long)> _partOneTestData = new List<(int, int, long)>()
+	{
+		(1,2000,8685429),
+		(10,2000,4700978),
+		(100,2000,15273692),
+		(2024,2000,8667524),
+	};
 
 	#endregion
 
