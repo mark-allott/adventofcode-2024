5d85c8cc609034d6b41392da60322a77de22729b
Mark Allott <github.mark-allott@malsoft.com>
2025-04-16 13:16:55 +0100

    Refactoring for part two and new methods to solve part two

--- AdventOfCode/Models/LanParty.cs
+++ AdventOfCode/Models/LanParty.cs
@@ -55,25 +55,29 @@ internal class LanParty
 		//	Iterate over the computer names alphabetically
 		foreach (var computerName in _computerNamesAndConnections.Keys.Order())
 		{
-			//	Get all links from "A" to "B"
-			var triplets = _computerNamesAndConnections[computerName]
-				.Select(s => new { A = computerName, B = s })
-				//	Get all links from "B" to "C", retain "A"
-				.SelectMany(s => _computerNamesAndConnections[s.B].Select(cs => new { A = s.A, B = s.B, C = cs }))
-				//	Filter for connections where "C" links back to "A"
-				.Where(q => _computerNamesAndConnections[q.C].Contains(computerName))
-				.ToList();
-
-			//	Convert the triplets to text and take only distinct paths
-			var links = triplets.Select(s => new List<string>() { s.A, s.B, s.C })
-				.Select(s => string.Join(",", s.Order()))
-				.Distinct()
-				.ToList();
-			result.AddRange(links);
+			result.AddRange(GetTripletFor(computerName));
 		}
 		//	There may be reverse paths we have found, so make sure only truly distinct ones are returned
 		return result.Distinct()
 			.Order()
+			.ToList();
+	}
+
+	private List<string> GetTripletFor(string computerName)
+	{
+		//	Get all links from "A" to "B"
+		var triplets = _computerNamesAndConnections[computerName]
+			.Select(s => new { A = computerName, B = s })
+			//	Get all links from "B" to "C", retain "A"
+			.SelectMany(s => _computerNamesAndConnections[s.B].Select(cs => new { A = s.A, B = s.B, C = cs }))
+			//	Filter for connections where "C" links back to "A"
+			.Where(q => _computerNamesAndConnections[q.C].Contains(computerName))
+			.ToList();
+
+		//	Convert the triplets to text and take only distinct paths
+		return triplets.Select(s => new List<string>() { s.A, s.B, s.C })
+			.Select(s => string.Join(",", s.Order()))
+			.Distinct()
 			.ToList();
 	}
 
@@ -92,5 +96,109 @@ internal class LanParty
 			.ToList();
 		return x;
 	}
+
+	/// <summary>
+	/// Get the password for the LAN Party based on largest network
+	/// </summary>
+	/// <returns></returns>
+	public string GetLanPartyPassword()
+	{
+		var candidates = GetNetworks();
+		return candidates.FirstOrDefault() ?? "";
+	}
+
+	/// <summary>
+	/// Holds lookups for network nodes and best connections found
+	/// </summary>
+	private Dictionary<string, List<string>> _lookups = null!;
+
+	/// <summary>
+	/// Finds all networks for the computers in the problem
+	/// </summary>
+	/// <returns>All networks, ordered by largest to smallest</returns>
+	private List<string> GetNetworks()
+	{
+		//	Initialise
+		_lookups = new Dictionary<string, List<string>>();
+		var results = new List<string>();
+
+		//	look for networks
+		_computerNamesAndConnections.Keys
+			.ToList()
+			.ForEach(n => results.AddRange(GetNetworkFor(n)));
+
+		//	Get the distinct results, order and return
+		results = results.Distinct()
+			.OrderByDescending(o => o.Length)
+			.ThenBy(o => o)
+			.ToList();
+		return results;
+	}
+
+	/// <summary>
+	/// Get the network of computers from the given list of computers
+	/// </summary>
+	/// <param name="computerList">The computer (or computers) in the current network</param>
+	/// <returns>All computers in the network that contain <paramref name="computerList"/></returns>
+	private List<string> GetNetworkFor(string computerList)
+	{
+		//	Do we have a suitable return value for this key?
+		if (_lookups.TryGetValue(computerList, out var result))
+			return result;
+
+		//	Split into list for lookups
+		var computers = computerList.Split(",").ToList();
+		var connections = _computerNamesAndConnections.Keys
+			//	Connections must contain ALL nodes
+			.Where(key => computers.All(cn => _computerNamesAndConnections[key].Contains(cn)))
+			//	Recombine computer names for iterative lookup
+			.Select(s => string.Join(",", new List<string>(computers) { s }.Order()))
+			.ToList();
+
+		//	If no connections were found, then we have the result
+		if (connections.Count == 0)
+			return AddLookup(computerList, computerList);
+
+		//	Perform a recurisive lookup for all new candidate connections
+		var results = connections
+			.Select(s => GetNetworkFor(s))
+			.SelectMany(m => m)
+			.Distinct()
+			.ToList();
+		//	Find the best result (or results)
+		var best = results.Where(q => q.Length == results.Max(m => m.Length))
+			.Distinct()
+			.ToList();
+		return AddLookup(computerList, best);
+	}
+
+	/// <summary>
+	/// Adds the <paramref name="value"/> to the dictionary for the given <paramref name="key"/>
+	/// </summary>
+	/// <param name="key">The key to be updated</param>
+	/// <param name="value">The value to be added</param>
+	/// <returns>The distinct entries for the <paramref name="key"/></returns>
+	private List<string> AddLookup(string key, string value)
+	{
+		if (!_lookups.TryGetValue(key, out var existing))
+			existing = new List<string>();
+		existing.Add(value);
+		return _lookups[key] = existing.Distinct().ToList();
+	}
+
+	/// <summary>
+	/// Adds the given <paramref name="values"/> to the dictionary for the specified <paramref name="key"/>
+	/// </summary>
+	/// <param name="key">The key to be updated</param>
+	/// <param name="values">Entries to be added</param>
+	/// <returns>The distinct entries for the <paramref name="key"/></returns>
+	private List<string> AddLookup(string key, IEnumerable<string> values)
+	{
+		if (!_lookups.TryGetValue(key, out var existing))
+			existing = new List<string>();
+		existing.AddRange(values);
+		return _lookups[key] = existing.Distinct().ToList();
+	}
+
 	#endregion
 }
