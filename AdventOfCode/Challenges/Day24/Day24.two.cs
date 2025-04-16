89f663a902d7673b5760a100251df3ab1d3f1fde
Mark Allott <github.mark-allott@malsoft.com>
2025-04-16 13:24:07 +0100

    Skeleton for day 24

---
+++ AdventOfCode/Challenges/Day24/Day24.two.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using AdventOfCode.Interfaces;
+using AdventOfCode.Models;
+
+namespace AdventOfCode.Challenges.Day24;
+
+public partial class Day24
+	: AbstractDailyChallenge, IAutoRegister, IPartTwoTestable
+{
+	#region Overrides to run part two of challenge
+
+	/// <summary>
+	/// Overrides the base implementation to provide the solution for part two
+	/// </summary>
+	/// <returns></returns>
+	protected override bool PartTwo()
+	{
+		LoadAndReadFile();
+		var result = "N/A";
+		PartTwoResult = $"{ChallengeTitle} result = {result}";
+		return true;
+	}
+
+	#endregion
+
+	#region IPartTwoTestable implementation
+
+	public void PartTwoTest()
+	{
+	}
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+}
